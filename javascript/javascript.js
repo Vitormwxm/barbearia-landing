2097556ea0194e72a63e95bc4a16c7983e107584
Vitor Martiniano <vitormwxm@gmail.com>
2025-04-04 02:24:01 -0300

    adicionado animacao quando clica no scroll

---
+++ javascript/javascript.js
@@ -0,0 +1,19 @@
+const linkInternos = document.querySelectorAll(".container-menu a[href^='#']")
+
+
+function scrollLink(event) {
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute('href');
+    const linkClicado = document.querySelector(href)
+
+    linkClicado.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+    });
+}
+
+
+
+linkInternos.forEach((item) => {
+    item.addEventListener('click' , scrollLink)
+})
