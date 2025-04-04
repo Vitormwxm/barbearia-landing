58b527cedfbfa795b8338e485b1ad416ad760351
Vitor Martiniano <vitormwxm@gmail.com>
2025-04-04 02:29:36 -0300

    refatorando código

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,19 +1,24 @@
-const linkInternos = document.querySelectorAll(".container-menu a[href^='#']")
 
+function initScroll(){   
+    const linkInternos = document.querySelectorAll(".container-menu a[href^='#']")
 
-function scrollLink(event) {
-    event.preventDefault()
-    const href = event.currentTarget.getAttribute('href');
-    const linkClicado = document.querySelector(href)
+    function scrollLink(event) {
+        event.preventDefault()
+        const href = event.currentTarget.getAttribute('href');
+        const linkClicado = document.querySelector(href)
+    
+        linkClicado.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    }
+    
+    
+    
+    linkInternos.forEach((item) => {
+        item.addEventListener('click' , scrollLink)
+    })
 
-    linkClicado.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-    });
 }
 
-
-
-linkInternos.forEach((item) => {
-    item.addEventListener('click' , scrollLink)
-})
+initScroll()
